644fa1a477ca97f57cc848f1d6bd00f464544671
Luka Bitsadze <bitsadzeluka@gmail.com>
2024-07-26 02:17:34 +0400

    final version

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  currentDate: Date = new Date();
+
+  constructor(private sharedService: SharedService) { }
+
+  ngOnInit() {
+    this.showTime();
+
+  }
+
+  showTime(){
+    setInterval(() => {
+      this.currentDate = new Date();
+      this.sharedService.updateCurrentDate(this.currentDate);
+    }, 1000);
+
+  }
+}
